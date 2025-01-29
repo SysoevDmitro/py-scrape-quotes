4811f22947f4ca7d10e965fe6f4ffbda8bdd923e
Dmitro Sysoev <dimasysoev20000@gmail.com>
2025-01-29 21:52:40 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -11,23 +11,46 @@ class Quote:
     tags: list[str]
 
 
-def main(output_csv_path: str) -> None:
-    url = requests.get("https://quotes.toscrape.com")
-    soup = BeautifulSoup(url.text, "html.parser")
+def fetch_page(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return BeautifulSoup(response.text, "html.parser")
 
-    quotes = soup.findAll("span", attrs={"class": "text"})
-    authors = soup.findAll("small", attrs={"class": "author"})
-    tags = soup.findAll("div", attrs={"class": "tags"})
 
+def extract_quotes(soup):
+    quotes = soup.find_all("span", attrs={"class": "text"})
+    authors = soup.find_all("small", attrs={"class": "author"})
+    tags = soup.find_all("div", attrs={"class": "tags"})
+    return zip(quotes, authors, tags)
+
+
+def write_csv(quotes_data, output_csv_path):
     with open(output_csv_path, "w", encoding="utf-8", newline="") as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=",")
         csv_writer.writerow(["text", "author", "tags"])
+        for quote, author, tag in quotes_data:
 
-        for quote, author, tag in zip(quotes,
-                                      authors,
-                                      tags):
-            csv_writer.writerow([quote.text, author.text,
-                                 tag.text.split()[1:]])
+            tags = tag.text.split()[1:]
+            csv_writer.writerow([quote.text, author.text, tags])
+
+
+def main(output_csv_path: str) -> None:
+    base_url = "https://quotes.toscrape.com"
+    current_page = 1
+    all_quotes = []
+
+    while True:
+        url = f"{base_url}/page/{current_page}"
+        soup = fetch_page(url)
+        parsed_quotes = extract_quotes(soup)
+        all_quotes.extend(parsed_quotes)
+        next_button = soup.find("li", class_="next")
+        if next_button:
+            current_page += 1
+        else:
+            break
+
+    write_csv(all_quotes, output_csv_path)
 
 
 if __name__ == "__main__":
